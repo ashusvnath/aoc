5572e4ce6a0fffdc4ba43458dccb6dd3eef2902d
Suhas Vishwanath <vhasus@localhost>
2022-12-02 03:38:14 +0530

    Cleanup and optimizations

--- day1.rb
+++ day1.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require 'logger'
 logger = Logger.new(STDERR)
 logger.level = Logger::ERROR
@@ -7,16 +8,14 @@ input = nil
 OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} [options]"
 
-  opts.on("-v", "--[no-]verbose", "Run verbosely") do
-    logger.level = Logger::DEBUG
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    logger.level = v ? Logger::DEBUG : Logger::ERROR
   end
 
   opts.on("-f [FILE]", "--file [FILE_PATH]", "Path to input file ") do |file_path|
     input = file_path
   end
 end.parse!
-
-
 
 if input == nil
     print "Name of file: "
@@ -25,27 +24,25 @@ end
 
 logger.debug "Input file:" + input
 
-
-data = File.read(input).lines.map(&:chomp)
 all_elf_data = {}
-part1_max = 0
 part2_max = []
 num_elves = 1
-current_calories = []
-result = data.reduce { |elves, l|
-    if l != ""
-        current_calories << l.to_i
-    else
-        elf_data = { calories: current_calories, total: current_calories.sum }
+current_elf_calories = []
+
+File.read(input).lines.each do |line|
+    calories = line.chomp.to_i
+    current_elf_calories << calories
+    if calories == 0
+        elf_data = { calories: current_elf_calories, total: current_elf_calories.sum }
         logger.debug "#{num_elves}, #{elf_data[:total]}"
         all_elf_data[num_elves] = elf_data
         num_elves += 1
-        current_calories = []
-        part1_max = elf_data[:total] > part1_max ? elf_data[:total] : part1_max
+        current_elf_calories = []
         part2_max << elf_data[:total]
-        part2_max = part2_max.sort.reverse.take(3)
+        part2_max = part2_max.sort{|x,y| y <=> x}.take(3)
+        logger.debug "Part1 max so far: #{part2_max[0]}"
+        logger.debug "Part2 max so far: #{part2_max}"
     end
-    all_elf_data
-}
-puts "Part 1 max: #{part1_max}"
+end
+puts "Part 1 max: #{part2_max[0]}"
 puts "Part 2 max: #{part2_max.sum}"
