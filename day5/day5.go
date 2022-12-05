3f929aa4806ebd1658f3124270468d524658b785
Suhas Vishwanath <vhasus@localhost>
2022-12-05 23:53:23 +0530

    Day5 both parts working

--- day5/day5.go
+++ day5/day5.go
@@ -37,7 +37,7 @@ func readFile(filepath string) []byte {
 	return data
 }
 
-func applyInstruction(inst _instruction, stacks map[string][]string) {
+func applyInstructionPart1(inst _instruction, stacks map[string][]string) {
 	//read
 	from := stacks[inst.from]
 	to := stacks[inst.to]
@@ -55,7 +55,23 @@ func applyInstruction(inst _instruction, stacks map[string][]string) {
 	stacks[inst.to] = to
 }
 
-func readStack(currentStackString string) (map[string][]string, []string) {
+func applyInstructionPart2(inst _instruction, stacks map[string][]string) {
+	//read
+	from := stacks[inst.from]
+	to := stacks[inst.to]
+
+	//transform
+	stackLen := len(from)
+	taken := from[stackLen-inst.count:]
+	from = from[0 : stackLen-inst.count]
+	to = append(to, taken...)
+
+	//store
+	stacks[inst.from] = from
+	stacks[inst.to] = to
+}
+
+func readStack(currentStackString string) ([2]map[string][]string, []string) {
 	currentStackLines := strings.Split(currentStackString, "\n")
 	currentStackLinesLen := len(currentStackLines)
 	labelsRegexp := regexp.MustCompile(`\d+`)
@@ -64,11 +80,13 @@ func readStack(currentStackString string) (map[string][]string, []string) {
 	log.Printf("Indices: %v", indices)
 
 	//Initialize stacks
-	stacks := make(map[string][]string, len(indices))
+	stacks0 := make(map[string][]string, len(indices))
+	stacks1 := make(map[string][]string, len(indices))
 	stackIds := make([]string, len(indices))
 	for stackId, idxs := range indices {
 		stackIds[stackId] = currentStackLines[currentStackLinesLen-1][idxs[0]:idxs[1]]
-		stacks[stackIds[stackId]] = make([]string, 0)
+		stacks0[stackIds[stackId]] = make([]string, 0)
+		stacks1[stackIds[stackId]] = make([]string, 0)
 	}
 
 	//Read each line of output in reverse order
@@ -79,14 +97,15 @@ func readStack(currentStackString string) (map[string][]string, []string) {
 			stackId := stackIds[iter]
 			crateName := currentStackLines[i][idxs[0]:idxs[1]]
 			if crateName != " " {
-				stacks[stackId] = append(stacks[stackIds[iter]], crateName)
+				stacks0[stackId] = append(stacks0[stackIds[iter]], crateName)
+				stacks1[stackId] = append(stacks1[stackIds[iter]], crateName)
 			}
 		}
 	}
-	return stacks, stackIds
+	return [2]map[string][]string{stacks0, stacks1}, stackIds
 }
 
-func processInstructions(input string, stacks map[string][]string, stackIds []string) {
+func processInstructions(input string, stacks [2]map[string][]string, stackIds []string) {
 	instuructionPattern := regexp.MustCompile(`move (\d+) from (\d+) to (\d+)`)
 	instructions := strings.Split(input, "\n")
 	for _, instruction := range instructions {
@@ -96,7 +115,8 @@ func processInstructions(input string, stacks map[string][]string, stackIds []st
 		from := string(matches[2])
 		to := string(matches[3])
 		log.Printf("Instruction: move count:%d, from:%s , to:%s", count, from, to)
-		applyInstruction(_instruction{count, from, to}, stacks)
+		applyInstructionPart1(_instruction{count, from, to}, stacks[0])
+		applyInstructionPart2(_instruction{count, from, to}, stacks[1])
 		log.Printf("After apply: %v", stacks)
 	}
 }
@@ -113,9 +133,11 @@ func main() {
 
 	processInstructions(parts[1], stacks, stackIds)
 	for _, stackId := range stackIds {
-		part1Result += stacks[stackId][len(stacks[stackId])-1]
+		part1Result += stacks[0][stackId][len(stacks[0][stackId])-1]
+		part2Result += stacks[1][stackId][len(stacks[1][stackId])-1]
 	}
 
+	log.Printf("Stacks : %v", stacks)
 	fmt.Printf("Part 1: %s\n", part1Result)
 	fmt.Printf("Part 2: %s\n", part2Result)
 }
