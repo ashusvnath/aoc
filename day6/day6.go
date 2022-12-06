a68688ed7e74f740d76f7586a4fd68489da20d64
Suhas Vishwanath <vhasus@localhost>
2022-12-06 15:55:54 +0530

    Day6 part2 done

--- day6/day6.go
+++ day6/day6.go
@@ -35,32 +35,65 @@ func readFile(filepath string) []byte {
 	return data
 }
 
-func processLine(line string, lno int) {
-	chars := make(map[rune][]int)
+func processLineForSOP(line string, lno int) {
+	sopChars := make(map[rune][]int)
 	for idx, b := range line {
-		idxs, ok := chars[b]
+
+		idxs, ok := sopChars[b]
 		if !ok {
 			idxs = []int{}
 		}
 		idxs = append(idxs, idx)
-		chars[b] = idxs
+		sopChars[b] = idxs
+
 		if idx > 3 {
 			charToRemove := rune(line[idx-4])
 			log.Printf("Removing char %v at %d", charToRemove, idx)
-			found := chars[charToRemove]
-			if len(found) > 1 {
-				chars[charToRemove] = found[1:]
+			foundSop := sopChars[charToRemove]
+			if len(foundSop) > 1 {
+				sopChars[charToRemove] = foundSop[1:]
 			} else {
-				delete(chars, charToRemove)
+				delete(sopChars, charToRemove)
 			}
-			log.Printf("line: %d, chars:%v", lno, chars)
 		}
 
-		if len(chars) == 4 {
-			log.Printf("line: %d, found 4 keys at idx: %d (%v)", lno, idx, chars)
+		log.Printf("line: %d, SOP chars:%v", lno, sopChars)
+
+		if len(sopChars) == 4 {
+			log.Printf("line: %d, found 4 keys at idx: %d (%v)", lno, idx, sopChars)
 			fmt.Printf("line: %d, first loc of 4 non-repeating chars: %d\n", lno, idx+1)
 			break
 		}
+	}
+}
+
+func processLineForSOM(line string, lno int) {
+	somChars := make(map[rune][]int)
+	for idx, b := range line {
+		idxs, ok := somChars[b]
+		if !ok {
+			idxs = []int{}
+		}
+		idxs = append(idxs, idx)
+		somChars[b] = idxs
+		if idx > 13 {
+			charToRemove := rune(line[idx-14])
+			log.Printf("Removing char %v at %d", charToRemove, idx)
+			foundSom := somChars[charToRemove]
+			if len(foundSom) > 1 {
+				somChars[charToRemove] = foundSom[1:]
+			} else {
+				delete(somChars, charToRemove)
+			}
+		}
+		log.Printf("line: %d, SOM chars: %v", lno, somChars)
+
+		if len(somChars) == 14 {
+			log.Printf("line: %d, found 14 keys at idx: %d (%v)", lno, idx, somChars)
+			fmt.Printf("line: %d, first loc of 14 non-repeating chars: %d\n", lno, idx+1)
+			break
+		}
+
 	}
 }
 
@@ -69,6 +102,7 @@ func main() {
 	lines := strings.Split(string(data), "\n")
 	for lno, line := range lines {
 		log.Printf("line:%d, data:%s", lno, line)
-		processLine(line, lno)
+		processLineForSOP(line, lno)
+		processLineForSOM(line, lno)
 	}
 }
