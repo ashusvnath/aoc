cdd1db74807967c1253bd77e9f3044337146abd1
Suhas Vishwanath <vhasus@localhost>
2023-01-18 01:18:53 +0530

    Day8 part 2 working. Trimming whitespaces at Eof

--- day8/main.go
+++ day8/main.go
@@ -65,21 +65,30 @@ func main() {
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
-	parsedData := Parse(data)
-	part1 := Part1(parsedData)
-	fmt.Printf("Part1 : %v\n", part1)
+	mat, idxsByHeight := Parse(data)
+	part1 := Part1(mat)
+	part2 := Part2(mat, idxsByHeight)
+	fmt.Printf("Part1 : %v\nPart2 : %v\n", part1, part2)
 }
 
-func Parse(data []byte) [][]int {
+func Parse(data []byte) ([][]int, map[int][]int) {
 	lines := strings.Split(strings.TrimSuffix(string(data), "\n"), "\n")
-	matrix := make([][]int, len(lines))
+	l := len(lines)
+	matrix := make([][]int, l)
+	idxsByHeight := make(map[int][]int)
 	for row, lineString := range lines {
-		matrix[row] = make([]int, len(lines))
+		matrix[row] = make([]int, l)
 		for col, c := range lineString {
-			matrix[row][col] = int(c) - '0'
+			height := int(c) - '0'
+			matrix[row][col] = height
+			idx := row*l + col
+			if row == 0 || col == 0 || row == l-1 || col == l-1 {
+				continue
+			}
+			idxsByHeight[height] = append(idxsByHeight[height], idx)
 		}
 	}
-	return matrix
+	return matrix, idxsByHeight
 }
 
 func Part1(data [][]int) int {
@@ -113,10 +122,67 @@ func Part1(data [][]int) int {
 			}
 		}
 	}
-	fmt.Printf("Seen : %v\n", visible)
 	return 4*l + len(visible)
 }
 
-func Part2(data [][]int) int {
-	return 0
+func Part2(data [][]int, idxsByHeight map[int][]int) int {
+	maxScenicScore := -1
+	maxIdx := -1
+	for height := 9; height >= 1; height-- {
+		idxs := idxsByHeight[height]
+		if idxs == nil {
+			continue
+		}
+		log.Printf("Checking height: %v, Idxs: %v", height, idxs)
+		for _, idx := range idxs {
+			score, parts := scenicScore(idx, data)
+			if score > maxScenicScore {
+				log.Printf("Part2: Max score beaten at: Idx: %d, score: %d, parts: %v", idx, score, parts)
+				maxScenicScore = score
+				maxIdx = idx
+			}
+		}
+	}
+	log.Printf("Max idx: %v", maxIdx)
+	return maxScenicScore
+}
+
+func scenicScore(idx int, mat [][]int) (int, [4]int) {
+	l := len(mat)
+	x, y := idx/l, idx%l
+	h := mat[x][y]
+	checkRight, checkUp, checkLeft, checkDown := true, true, true, true
+	rightScore, upScore, leftScore, downScore := 1, 1, 1, 1
+	for i := 1; i < l && (checkLeft || checkDown || checkRight || checkUp); i++ {
+		if checkDown && x+i < l && mat[x+i][y] >= h {
+			downScore = i
+			checkDown = false
+		}
+		if checkRight && y+i < l && mat[x][y+i] >= h {
+			rightScore = i
+			checkRight = false
+		}
+		if checkUp && x-i > -1 && mat[x-i][y] >= h {
+			upScore = i
+			checkUp = false
+		}
+		if checkLeft && y-i > -1 && mat[x][y-i] >= h {
+			leftScore = i
+			checkLeft = false
+		}
+	}
+	if checkLeft {
+		leftScore = y
+	}
+	if checkRight {
+		rightScore = l - y - 1
+	}
+	if checkUp {
+		upScore = x
+	}
+	if checkDown {
+		downScore = l - x - 1
+	}
+	score := leftScore * rightScore * upScore * downScore
+	return score, [4]int{upScore, leftScore, rightScore, downScore}
 }
